484a00272ac5a014e47766fa5f2dcd8f535ae26a
Jordan Love <jnlove314@gmail.com>
2019-02-28 14:20:46 -0600

    Add first two images of slideshow

--- Slideshow.js
+++ Slideshow.js
@@ -1,6 +1,13 @@
+let img1 = "";
+let img2 = "";
+let img3 = "";
+let img4 = "";
+let img5 = "";
+
 function next()
 {
-  alert("You have selected next!");
+  let kittenPic = document.getElementById("image")
+  kittenPic.src = "https://www.hillspet.com/content/dam/cp-sites/hills/hills-pet/en_us/exported/cat-care/new-pet-parent/images/mother-cat-and-kitten-sleeping.jpg";
 }
 
 function previous()
